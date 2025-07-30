8ef1e954ffc4a60af5422abed93f4bbd221ba79e
box-7 <hayainada1981@yahoo.co.jp>
2025-07-30 12:50:41 +0900

    Add: app/apiディレクトリに全件取得APIを書き換え

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -13,3 +13,17 @@
 
 //         return res.status(200).json(data);
 // }
+
+import { supabase } from "@/utils/supabaseClient";
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request, res: NextApiResponse) {
+        const { data, error } = await supabase.from("posts").select("*");
+
+        if (error) {
+                return NextResponse.json(error);
+
+        }
+        return NextResponse.json(data, { status: 200 });
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import ArticleList from "./components/ArticleList";
 export default async function Home() {
         const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-        const res = await fetch(`${API_URL}/api`, { cache: "no-store" });
+        const res = await fetch(`${API_URL}/api/blog`, { cache: "no-store" });
         if (!res.ok) {
                 throw new Error("Failed to fetch articles");
         }
