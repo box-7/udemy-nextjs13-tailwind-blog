6adfaa6b6bbed1045b8393c113654007265e235b
box-7 <hayainada1981@yahoo.co.jp>
2025-07-30 12:37:08 +0900

    Add: Supabaseで削除機能実装

--- src/app/components/DeleteButton.tsx
+++ src/app/components/DeleteButton.tsx
@@ -1,7 +1,7 @@
 // このコンポーネントはサーバーコンポーネントではなくクライアントコンポーネントとして定義
 "use client";
 import React from 'react'
-import { deleteArticle } from '@/blogAPI';
+// import { deleteArticle } from '@/blogAPI';
 import { useRouter } from 'next/navigation';
 type DeleteButtonProps = {
         id: string;
@@ -10,7 +10,12 @@ type DeleteButtonProps = {
 const DeleteButton = ({ id }: DeleteButtonProps) => {
         const router = useRouter();
         const handleDelete = async () => {
-                await deleteArticle(id);
+                // await deleteArticle(id);
+
+                const API_URL = process.env.NEXT_PUBLIC_API_URL;
+                await fetch(`${API_URL}/api/${id}`, {
+                        method: "DELETE",
+                });
                 router.push("/");
                 // 現在のページのデータを**再取得（リフレッシュ）
                 // APIでデータを更新・削除した後、最新の状態を表示したいときに使う
@@ -19,7 +24,7 @@ const DeleteButton = ({ id }: DeleteButtonProps) => {
 
         return (
                 <div className="bg-red-500 hover:bg-red-600 rounded-md font-bold py-2 px-5 inline cursor-pointer"
-                onClick={handleDelete}>
+                        onClick={handleDelete}>
                         削除
                 </div>
         )
--- src/pages/api/[id].ts
+++ src/pages/api/[id].ts
@@ -6,11 +6,27 @@ export default async function handler(
         res: NextApiResponse
 ) {
         const {id} = req.query;
-        const {data, error } = await supabase.from("posts").select("*").eq("id", id).single();
 
-        if (error) {
-                return res.status(500).json({ error: error.message });
+        switch (req.method) {
+                case "GET":
+                        const {data, error } = await supabase.from("posts").select("*").eq("id", id).single();
+
+                        if (error) {
+                                return res.status(500).json({ error: error.message });
+                        }
+
+                        return res.status(200).json(data);
+
+                case "DELETE":
+                        const { error: deleteError } = await supabase.from("posts").delete().eq("id", id);
+
+                        if (deleteError) {
+                                return res.status(500).json({ error: deleteError.message });
+                        }
+                        // return res.status(200).json({ message: "Post deleted successfully" });
+
+                        // 204が適切 正常に削除されたが、返すコンテンツはない
+                        return res.status(204).json({ message: "Post deleted successfully" });
+
         }
-
-        return res.status(200).json(data);
 }
